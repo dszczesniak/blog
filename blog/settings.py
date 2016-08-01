d546141d529be8742a58a751d67601f378d2339c
dszczesniak <dszczesniak@poczta.pl>
2016-08-01 23:24:57 +0200

    additional files and changes for heroku

--- blog/settings.py
+++ blog/settings.py
@@ -140,3 +140,20 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static_in_env", "media_root")
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+
+#Dla Heroku
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
