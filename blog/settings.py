50489b9f255eb8026104a46b88998c7123652dbe
dszczesniak <dszczesniak@poczta.pl>
2016-09-01 22:18:45 +0200

    SendMessageRepairSec

--- blog/settings.py
+++ blog/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
